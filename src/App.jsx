cae643476b493c38895e9a61f2909c00adade8ce
Sahitya-Dwivedi <sahityadwivedi515@gmail.com>
2025-05-03 22:52:44 +0530

    fix: set document title to "PDF Magic" in App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Features from "./components/features";
 import Footer from "./components/footer";
 
 function App() {
+  // Set document title to PDF Magic
+  document.title = "PDF Magic";
+  
   return (
     <div className="App bg-gradient-to-br from-gray-900 to-black text-white w-screen min-h-screen flex flex-col">
       <Navbar />
