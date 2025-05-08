9c557c79fc229933da2dd14ccefb8188a4cfef79
Sahitya-Dwivedi <sahityadwivedi515@gmail.com>
2025-05-08 22:52:33 +0530

    feat: implement routing with react-router-dom and update App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
-import Hero from "./components/hero";
-import Features from "./components/features";
+import Home from "./pages/home";
+import About from "./pages/about";
 import Footer from "./components/footer";
 
 function App() {
@@ -9,12 +10,17 @@ function App() {
   document.title = "PDF Magic";
 
   return (
-    <div className="App bg-gradient-to-br from-gray-900 to-black text-white w-screen min-h-screen flex flex-col overflow-x-hidden">
-      <Navbar />
-      <Hero />
-      <Features />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App bg-gradient-to-br from-gray-900 to-black text-white w-screen min-h-screen flex flex-col overflow-x-hidden">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
