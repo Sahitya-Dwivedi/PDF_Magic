0b79e83ba18c8c8ab65dd3c4a4da1209d8be2770
Sahitya-Dwivedi <sahityadwivedi515@gmail.com>
2025-05-18 15:17:13 +0530

    feat: refactor App component to use useLocation for conditional Navbar rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Footer from "./components/footer";
 import PdfViewer from "./pages/PdfViewer";
 
-function App() {
+function AppContent() {
+  const location = useLocation();
   // Set document title to PDF Magic
   document.title = "PDF Magic";
   // Set favicon
@@ -16,17 +17,24 @@ function App() {
   favicon.href = "/public/pdf_magic_favicon.ico";
 
   return (
+    <div className="App bg-gradient-to-br from-gray-900 to-black text-white w-screen min-h-screen flex flex-col overflow-x-hidden">
+      {/* Only show Navbar if not on /pdfviewer */}
+      {location.pathname !== "/pdfviewer" && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/pdfviewer" element={<PdfViewer />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+function App() {
+  return (
     <Router>
-      <div className="App bg-gradient-to-br from-gray-900 to-black text-white w-screen min-h-screen flex flex-col overflow-x-hidden">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/pdfviewer" element={<PdfViewer />} />
-        </Routes>
-        <Footer />
-      </div>
+      <AppContent />
     </Router>
   );
 }
